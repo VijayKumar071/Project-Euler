c4d585bd52631ac9ea131243cd65f56765844118
Vijay Kumar <vijayk071@gmail.com>
2017-08-25 21:26:20 +0530

    Update and rename palin.c to euler4.c

--- euler4.c
+++ euler4.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+/*A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.*/
 int palin(int n)
 {
 	 int rem=0,digit,no=n;
